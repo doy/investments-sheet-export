fab9d941cbcaa3181b7ccb9107d84aa72ee655d8
Jesse Luehrs <doy@tozt.net>
2019-08-27 03:46:40 -0400

    also track income reported on tax returns

---
+++ data/tax_returns.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS taxes;
+DROP TABLE IF EXISTS tax_returns;
+
+CREATE TABLE tax_returns (
+    year date PRIMARY KEY,
+    total_income bigint,
+    total_tax bigint
+);
--- data/taxes.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS taxes;
-
-CREATE TABLE taxes (
-    year date PRIMARY KEY,
-    total_tax bigint
-);
